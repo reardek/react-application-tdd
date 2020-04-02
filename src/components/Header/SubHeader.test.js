9a2ae7d51d41c321b4bc7730676a16dcd2b8db17
Jędrzej Kowaliński <jjkowalinski@yahoo.com>
2020-04-02 13:02:03 +0200

    testing

---
+++ src/components/Header/SubHeader.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SubHeader, { Title, SubHeaderButton } from './SubHeader';
+
+describe('the Subheader component', () => {
+    it('should render', () => {
+        const title = 'Test Application';
+        const component = shallow(<SubHeader title={title} />)
+        expect(component.find(Title).text()).toEqual(title);
+    });
+
+    it('should render with a dynamic title ', () => {
+        const component = shallow(<SubHeader title='Test Application' />);
+
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should render with a buttons and handle the onClick events', () => {
+        const mockGoBack = jest.fn();
+        const mockOpenForm = jest.fn();
+        const component = shallow(<SubHeader goBack={mockGoBack} openForm={mockOpenForm} />)
+
+        const goBackButton = component.find(SubHeaderButton).at(0);
+        expect(goBackButton.exists()).toBe(true);
+        
+        const openFormButton = component.find(SubHeaderButton).at(1);
+        expect(openFormButton.exists()).toBe(true);
+
+        goBackButton.simulate('click');
+        expect(mockGoBack).toHaveBeenCalled();
+
+        openFormButton.simulate('click');
+        expect(mockOpenForm).toHaveBeenCalled();
+    });
+});
